f05092e9f2bab9e2e8f0c4d75cd1eb36f4af1272
jonlinnet28 <jonlinnet28@gmail.com>
2026-08-31 12:51:49 +0200

    Exercise 2

--- NotRunescape/obj/Debug/net10.0/NotRunescape.AssemblyInfo.cs
+++ NotRunescape/obj/Debug/net10.0/NotRunescape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotRunescape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3a25ecce6f5d0ff3bb8e946b23bd168c75d9b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7695af5eb2055f28a2ed8cd1b0adf8884876f5ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotRunescape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotRunescape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
