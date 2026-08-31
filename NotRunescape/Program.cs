9dc4b0f4e23184d35f35fd3a96297beea278ab8c
murik <dennyisgodxd@gmail.com>
2026-08-31 12:28:13 +0200

    Finished exercise 1

--- NotRunescape/Program.cs
+++ NotRunescape/Program.cs
@@ -8,6 +8,17 @@ Console.WriteLine("=== OSRS Boss & Combat Tracker ===");
 
 while (true)
 {
+    Console.Write("What is your name: ");
+    string name = Console.ReadLine() ?? "";
+    
+    if (name.Length >= 1)
+        Console.WriteLine($"Welcome to Gielinor, {name}!");
+    else
+    {
+        Console.WriteLine("You don't have a name!");
+        Environment.Exit(1);
+    }
+    
     Console.WriteLine($"\n[HP: {player.CurrentHp}/{player.MaxHp} | Gold: {player.Gold} GP]");
     Console.Write("[1] Log Boss Kill  [2] View Drop Log  [3] View Inventory  [4] Drop Item  [99] Fight Hill Giant  [0] Exit\nChoice: ");
     var input = Console.ReadLine()?.Trim();
