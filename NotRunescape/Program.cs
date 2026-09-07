6ab803d405c1f42a3f838a4ba475172d33158506
KennieAnker92 <155892051+KennieAnker92@users.noreply.github.com>
2026-09-07 07:37:48 +0200

    feat: add drop analytics for unique drops

--- NotRunescape/Program.cs
+++ NotRunescape/Program.cs
@@ -20,7 +20,7 @@ player.SetStartingGold(100);
 while (true)
 {
     Console.WriteLine($"\n[HP: {player.CurrentHp}/{player.MaxHp} | Gold: {player.Gold} GP]");
-    Console.Write("[1] Log Boss Kill  [2] View Drop Log  [3] View Inventory  [4] Drop Item  [5] Rest at Lumbridge  [6] View High Scores [7] Visit Store  [99] Fight Hill Giant  [0] Exit\nChoice: ");
+    Console.Write("[1] Log Boss Kill  [2] View Drop Log  [3] View Inventory  [4] Drop Item  [5] Rest at Lumbridge  [6] View High Scores [7] Visit Store [8] Drop Statistics [99] Fight Hill Giant  [0] Exit\nChoice: ");
     var input = Console.ReadLine()?.Trim();
 
     if (input == "0") break;
@@ -75,6 +75,10 @@ while (true)
     else if (input == "7")
     {
         store.OpenStore(player);
+    }
+    else if (input == "8")
+    {
+        DropAnalytics.DisplayDropStatistics(bossLogs);
     }
 }
 
